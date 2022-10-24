42d47d7bc72da8370f8034f79efb6524dfe28546
Thitiwas <thitiwas.nn@gmail.com>
2022-10-24 13:28:03 +0700

    first

---
+++ src/main/java/com/thitiwas/workshop/similar_stuff/SimilarStuffApplication.java
@@ -0,0 +1,13 @@
+package com.thitiwas.workshop.similar_stuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarStuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thitiwas/workshop/similar_stuff/SimilarStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thitiwas.workshop.similar_stuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
