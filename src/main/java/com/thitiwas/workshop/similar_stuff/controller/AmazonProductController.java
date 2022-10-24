96a9f3d911ef36b68a22302db0fa6adb3a032c0f
Thitiwas <thitiwas.nn@gmail.com>
2022-10-24 14:08:55 +0700

    up

---
+++ src/main/java/com/thitiwas/workshop/similar_stuff/controller/AmazonProductController.java
@@ -0,0 +1,36 @@
+package com.thitiwas.workshop.similar_stuff.controller;
+
+
+import com.thitiwas.workshop.similar_stuff.model.AmazonProductM;
+import com.thitiwas.workshop.similar_stuff.service.AmazonService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/amazon-product")
+@Slf4j
+public class AmazonProductController {
+    private final AmazonService amazonService;
+
+    @Autowired
+    public AmazonProductController(AmazonService amazonService) {
+        this.amazonService = amazonService;
+    }
+
+    @GetMapping("/v1/{productId}")
+    @Transactional
+    public ResponseEntity<AmazonProductM> getOne(@PathVariable("productId") String productId) {
+
+        Optional<AmazonProductM> amazonProduct = amazonService.findById(productId);
+
+        if (amazonProduct.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(amazonProduct.get());
+    }
+}
